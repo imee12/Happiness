dee1ab7cb96b7eece628f831e04f9fdc7fdcdfa0
Imee Cuison <imee@dynepic.com>
2015-08-14 10:07:44 -0400

    first commit

---
+++ HappinessStan/HappinessViewController.swift
@@ -0,0 +1,56 @@
+//
+//  HappinessViewController.swift
+//  HappinessStan
+//
+//  Created by Imee Cuison on 8/10/15.
+//  Copyright (c) 2015 Imee Cuison. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController, FaceViewDataSource
+{
+    var happiness: Int = 75 {
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            println("happiness = \(happiness)")
+            updateUI ()
+        }
+    }
+
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+        
+    }
+    
+    private func updateUI()
+    {
+        faceView.setNeedsDisplay()
+        
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50
+    }
+}
